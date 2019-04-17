cb08728c27a31c852dcf074cb697e522ad5e43cb
Xiaoting Fu <xiaot.fu@gmail.com>
2019-04-17 18:12:22 -0400

    update backend sturcture

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -71,15 +71,15 @@ function get_bookcontent(story, res) {
     const pyprog = spawn('python', ['../gen_backend/story_preprocess.py']);
     pyprog.stdout.on('data', function (data) {
         //console.log(data.toString());
-        var storyfile = '../db/output.txt';
-        var story_content;
+        // var storyfile = '../db/output.txt';
+        var story_content = data;
         // First I want to read the file
-        fs.readFile(storyfile, function read(err, data) {
-            if (err) {
-                throw err;
-            }
-            story_content = data;
-        });
+        // fs.readFile(storyfile, function read(err, data) {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     story_content = data;
+        // });
         console.log(story_content.toString())
         var sendfile = {"story": story_content.toString()};
         res.send(JSON.stringify(sendfile));
