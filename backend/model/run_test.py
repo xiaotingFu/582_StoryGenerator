e05db4471e92a622ce76864a53e541d73b32cc39
Xiaoting Fu <xiaot.fu@gmail.com>
2019-04-24 15:04:52 -0400

    update backend

--- backend/model/run_test.py
+++ backend/model/run_test.py
@@ -2,10 +2,13 @@
 import subprocess
 import sys
 import os
+# import time
 print("Start running the story generator")
 
 p = subprocess.Popen(sys.executable, "model/run.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+p.wait()
+# time.sleep(90)
 for line in p.stdout.readlines():
     print(line)
-retval = p.wait()
+
 sys.stdout.flush()
