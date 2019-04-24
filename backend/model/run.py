09f820d137815d5c2c808b479e9ae512897f2b2d
Xiaoting Fu <xiaot.fu@gmail.com>
2019-04-23 21:31:04 -0400

    update readme

--- backend/model/run.py
+++ backend/model/run.py
@@ -3,8 +3,10 @@ import json
 import sys
 import subprocess
 subprocess.call("../gen_backend/final_story.py", shell=True)
+
+
 with open('../db/output.txt') as f:
     story = f.read()
     print(story)
-sys.stdout.flush()
+# sys.stdout.flush()
 
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -68,11 +68,20 @@ function get_bookcontent(story, res) {
     // const pyprog = spawn('python', ['../gen_backend/story_preprocess.py']);
     const pyprog = spawn('python', ['model/run.py']);
     pyprog.stdout.on('data', function (data) {
-        var story_content = data;
+        //console.log(data.toString());
+        var storyfile = '../db/output.txt';
+        var story_content = "";
+        // First I want to read the file
+        fs.readFile(storyfile, function read(err, data) {
+            if (err) {
+                throw err;
+            }
+            story_content = data;
+        });
         console.log(story_content.toString())
         var sendfile = {"story": story_content.toString()};
         res.send(JSON.stringify(sendfile));
-        res.end(data);
+        res.end('end');
     });
   });
 }
