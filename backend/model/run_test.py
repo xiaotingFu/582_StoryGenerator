12dde6d2dacbc10681c52037d5061942171105db
Xiaoting Fu <xiaot.fu@gmail.com>
2019-04-24 15:07:24 -0400

    update backend

--- backend/model/run_test.py
+++ backend/model/run_test.py
@@ -7,8 +7,10 @@ print("Start running the story generator")
 
 p = subprocess.Popen(sys.executable, "model/run.py", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 p.wait()
+with open('../db/output.txt') as f:
+    story = f.read()
+    print(story)
 # time.sleep(90)
-for line in p.stdout.readlines():
-    print(line)
-
+# for line in p.stdout.readlines():
+#     print(line)
 sys.stdout.flush()
